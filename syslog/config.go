1c7e67a6f4b348835d90538e3cb4df0ce5245847
yoloz <youloz@outlook.com>
2023-04-07 11:18:59 +0800

    output-syslog

--- syslog/config.go
+++ syslog/config.go
@@ -1,40 +1,127 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package syslog
 
 import (
 	"fmt"
 	"log/syslog"
+	"strings"
+
+	"github.com/elastic/beats/v7/libbeat/outputs/codec"
 )
 
-type syslogConfig struct {
-	// Number of worker goroutines publishing log events
-	workers int `config:"workers" validate:"min=1"`
-	// Max number of events in a batch to send to a single client
-	batchSize int `config:"batch_size" validate:"min=1"`
-	// Max number of retries for single batch of events
-	retryLimit int `config:"retry_limit"`
-	// protocol of network,like tcp or udp
-	protocol string `config:"protocol"`
-	// syslog server address
-	address string `config:"address"`
-	// The Priority is a combination of the syslog facility and severity.
-	priority int `config:"priority"`
-	//
-	tag string `config:"tag"`
+func SeverityPriority(severity string) (syslog.Priority, error) {
+	severity = strings.ToUpper(severity)
+	switch severity {
+	case "EMERG":
+		return syslog.LOG_EMERG, nil
+	case "ALERT":
+		return syslog.LOG_ALERT, nil
+	case "CRIT":
+		return syslog.LOG_CRIT, nil
+	case "ERR":
+		return syslog.LOG_ERR, nil
+	case "WARNING":
+		return syslog.LOG_WARNING, nil
+	case "NOTICE":
+		return syslog.LOG_NOTICE, nil
+	case "INFO":
+		return syslog.LOG_INFO, nil
+	case "DEBUG":
+		return syslog.LOG_DEBUG, nil
+	default:
+		return 0, fmt.Errorf("invalid syslog severity: %s", severity)
+	}
+}
+
+func FacilityPriority(facility string) (syslog.Priority, error) {
+	facility = strings.ToUpper(facility)
+	switch facility {
+	case "KERN":
+		return syslog.LOG_KERN, nil
+	case "USER":
+		return syslog.LOG_USER, nil
+	case "MAIL":
+		return syslog.LOG_MAIL, nil
+	case "DAEMON":
+		return syslog.LOG_DAEMON, nil
+	case "AUTH":
+		return syslog.LOG_AUTH, nil
+	case "SYSLOG":
+		return syslog.LOG_SYSLOG, nil
+	case "LPR":
+		return syslog.LOG_LPR, nil
+	case "NEWS":
+		return syslog.LOG_NEWS, nil
+	case "UUCP":
+		return syslog.LOG_UUCP, nil
+	case "CRON":
+		return syslog.LOG_CRON, nil
+	case "AUTHPRIV":
+		return syslog.LOG_AUTHPRIV, nil
+	case "FTP":
+		return syslog.LOG_FTP, nil
+	case "LOCAL0":
+		return syslog.LOG_LOCAL0, nil
+	case "LOCAL1":
+		return syslog.LOG_LOCAL1, nil
+	case "LOCAL2":
+		return syslog.LOG_LOCAL2, nil
+	case "LOCAL3":
+		return syslog.LOG_LOCAL3, nil
+	case "LOCAL4":
+		return syslog.LOG_LOCAL4, nil
+	case "LOCAL5":
+		return syslog.LOG_LOCAL5, nil
+	case "LOCAL6":
+		return syslog.LOG_LOCAL6, nil
+	case "LOCAL7":
+		return syslog.LOG_LOCAL7, nil
+	default:
+		return 0, fmt.Errorf("invalid syslog facility: %s", facility)
+	}
+}
+
+type config struct {
+	Address  string       `config:"address"`
+	Proto    string       `config:"proto"`
+	Facility string       `config:"facility"`
+	Severity string       `config:"severity"`
+	Codec    codec.Config `config:"codec"`
 }
 
 var (
-	defaultConfig = syslogConfig{
-		//Workers:  1,
-		protocol: "tcp",
-		address:  "",
-		priority: int(syslog.LOG_INFO),
-		tag:      "",
+	defaultConfig = config{
+		Address:  "127.0.0.1:514",
+		Proto:    "udp",
+		Facility: "SYSLOG",
+		Severity: "INFO",
 	}
 )
 
-func (c *syslogConfig) Validate() error {
-	if c.address == "" {
-		return fmt.Errorf("%s", "Address is empty")
+func (c *config) Validate() error {
+	var err error
+	if _, err = FacilityPriority(c.Facility); err != nil {
+		return err
 	}
+	if _, err = SeverityPriority(c.Severity); err != nil {
+		return err
+	}
+
 	return nil
 }
---
+++ syslog/syslog.go
@@ -0,0 +1,153 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package syslog
+
+import (
+	"context"
+	"log/syslog"
+
+	"github.com/elastic/beats/v7/libbeat/beat"
+	"github.com/elastic/beats/v7/libbeat/common"
+	"github.com/elastic/beats/v7/libbeat/outputs"
+	"github.com/elastic/beats/v7/libbeat/outputs/codec"
+
+	"github.com/elastic/beats/v7/libbeat/logp"
+	"github.com/elastic/beats/v7/libbeat/publisher"
+)
+
+type syslogOutput struct {
+	address  string
+	proto    string
+	log      *logp.Logger
+	writer   *syslog.Writer
+	beat     beat.Info
+	observer outputs.Observer
+	codec    codec.Codec
+}
+
+func init() {
+	outputs.RegisterType("syslog", makeSyslog)
+}
+
+// MakeSyslog instantiates a new syslog output instance.
+func makeSyslog(_ outputs.IndexManager, beat beat.Info, observer outputs.Observer, cfg *common.Config) (outputs.Group, error) {
+	config := defaultConfig
+	if err := cfg.Unpack(&config); err != nil {
+		return outputs.Fail(err)
+	}
+
+	// disable bulk support in publisher pipeline
+	cfg.SetInt("bulk_max_size", -1, -1)
+
+	fo := &syslogOutput{
+		beat:     beat,
+		observer: observer,
+		log:      logp.NewLogger("syslog"),
+	}
+	if err := fo.init(beat, config); err != nil {
+		return outputs.Fail(err)
+	}
+
+	return outputs.Success(-1, 0, fo)
+}
+
+func (out *syslogOutput) init(beat beat.Info, c config) error {
+	out.address = c.Address
+	out.proto = c.Proto
+
+	severity, err := SeverityPriority(c.Severity)
+	if err != nil {
+		return err
+	}
+	facility, err := FacilityPriority(c.Facility)
+	if err != nil {
+		return err
+	}
+
+	out.writer, err = syslog.Dial(out.proto, out.address, severity|facility, "filebeat-syslog")
+	if err != nil {
+		return err
+	}
+
+	out.codec, err = codec.CreateEncoder(beat, c.Codec)
+	if err != nil {
+		return err
+	}
+
+	out.log.Info("Initialized syslog output. proto=%v address=%v severity=%v facility=%v",
+		out.proto, out.address, c.Severity, c.Facility)
+
+	return nil
+}
+
+// Implement Outputer:close,publish,string
+
+// Close close syslog writer.
+func (out *syslogOutput) Close() error {
+	return out.writer.Close()
+}
+
+// Publish sends events to the clients sink.
+func (out *syslogOutput) Publish(_ context.Context, batch publisher.Batch) error {
+	defer batch.ACK()
+
+	st := out.observer
+	events := batch.Events()
+	st.NewBatch(len(events))
+
+	dropped := 0
+	for i := range events {
+		event := &events[i]
+
+		serializedEvent, err := out.codec.Encode(out.beat.Beat, &event.Content)
+		if err != nil {
+			if event.Guaranteed() {
+				out.log.Errorf("Failed to serialize the event: %v", err)
+			} else {
+				out.log.Warnf("Failed to serialize the event: %v", err)
+			}
+
+			dropped++
+			continue
+		}
+
+		if _, err = out.writer.Write(append(serializedEvent, '\n')); err != nil {
+			st.WriteError(err)
+
+			if event.Guaranteed() {
+				out.log.Errorf("Sending event to remote syslog failed with: %v", err)
+			} else {
+				out.log.Warnf("Sending event to remote syslog failed failed with: %v", err)
+			}
+
+			dropped++
+			continue
+		}
+
+		st.WriteBytes(len(serializedEvent) + 1)
+	}
+
+	st.Dropped(dropped)
+	st.Acked(len(events) - dropped)
+
+	return nil
+}
+
+func (out *syslogOutput) String() string {
+	return "syslog(" + out.proto + "://" + out.address + ")"
+}
